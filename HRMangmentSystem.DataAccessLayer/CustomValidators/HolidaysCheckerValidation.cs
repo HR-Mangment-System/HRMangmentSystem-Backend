2cdb0d5cae10b8539cc849edf7d5c58a3471967b
omarosama203 <omarkukri203@gmail.com>
2024-05-08 16:52:35 +0300

    Trying to make a custom validation on attendance date (still trying , not finished yet )

---
+++ HRMangmentSystem.DataAccessLayer/CustomValidators/HolidaysCheckerValidation.cs
@@ -0,0 +1,58 @@
+﻿using HRMangmentSystem.DataAccessLayer.Context;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMangmentSystem.DataAccessLayer.CustomValidators
+{
+    public class HolidaysCheckerValidation : ValidationAttribute
+    {
+        private readonly HRMangmentCotext _hRMangmentCotext;
+
+        public HolidaysCheckerValidation(HRMangmentCotext hRMangmentCotext)
+        {
+            _hRMangmentCotext = hRMangmentCotext ?? throw new ArgumentNullException(nameof(hRMangmentCotext));
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateOnly date)
+            {
+                if (CheckIfHoliday(date))
+                {
+                    return new ValidationResult("The date is a holiday.");
+                }
+
+                if (CheckIfWeekendHoliday(date))
+                {
+                    return new ValidationResult("The date is a weekend holiday.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        private bool CheckIfHoliday(DateOnly date)
+        {
+            var holiday = _hRMangmentCotext.AnnualHolidays.FirstOrDefault(x => x.HolidayDate == date);
+            return holiday != null;
+        }
+
+        private bool CheckIfWeekendHoliday(DateOnly date)
+        {
+            var settings = _hRMangmentCotext.GeneralSettings.FirstOrDefault();
+            if (settings == null)
+            {
+                return false;
+            }
+
+            var holidayday1 = settings.WeeklyHoliday1;
+            var holidayday2 = settings.WeeklyHoliday2;
+            DayOfWeek firstDayOfWeek = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), holidayday1);
+            DayOfWeek secondDayOfWeek = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), holidayday2);
+
+            return date.DayOfWeek == firstDayOfWeek || date.DayOfWeek == secondDayOfWeek;
+        }
+    }
+}
